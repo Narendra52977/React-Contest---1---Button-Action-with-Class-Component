615638f17dbe7dfd09ee9935aae0ab2dbb07bbc8
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-10-22 17:43:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,18 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
-	    this.state={isClick:false,paragraph:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"};
-	    this.handleClick=this.handleClick.bind(this);
+	    this.state={isClick:false};
+	    
 	};
-	handleClick(){
-		this.setState=(state => ({
-      isClick: !state.isClick
-    }));
+	handleClick=()=>{
+		this.setState({isClick:true});
 	}
 
     render() {
 	
     	return(
     		<div id="main">
-				<button id="click" onClick={this.handleClick}>
+        <button id="click" onClick={this.handleClick}>
         para
       </button>
       {this.state.isClick ? <p id="para">{this.state.paragraph}</p> : null}
