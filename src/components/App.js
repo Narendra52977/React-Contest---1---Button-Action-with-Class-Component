5d8ccc5e6b25aa83786759b0414278a8aec51445
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-10-22 19:07:35 +0530

    edit

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,30 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	    this.state={isClick:false};
-	    
-	};
-	handleClick=()=>{
-		this.setState({isClick:true});
-	}
+  constructor(props) {
+    super(props);
+    this.state = { isClick: false };
+  }
+  handleClick = () => {
+    this.setState({ isClick: true });
+  };
 
-    render() {
-	
-    	return(
-    		<div id="main">
+  render() {
+    return (
+      <div id="main">
         <button id="click" onClick={this.handleClick}>
-        para
-      </button>
-      {this.state.isClick ? <p id="para">{this.state.paragraph}</p> : null}
-    		</div>
-    	);
-    }
+          para
+        </button>
+        {this.state.isClick === true ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        ) : null}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
