4240e6f9bf95000d2642e404c18f5960ceba0851
Narendra52977 <68866931+Narendra52977@users.noreply.github.com>
2020-10-23 18:20:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,10 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { isClick: null };
+    this.state = { isClick:false };
   }
   handleClick = () => {
-    this.setState({ isClick: <p id="para">
-            Hello, I've learnt to use the full-stack evaluation tool. This makes
-            me so happy
-          </p> });
+    this.setState({ isClick: true });
   };
 
   render() {
@@ -19,7 +16,10 @@ class App extends Component {
         <button id="click" onClick={this.handleClick}>
           para
         </button>
-        {this.state.isClick}
+        {this.state.isClick===true?<p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>:null}
       </div>
     );
   }
